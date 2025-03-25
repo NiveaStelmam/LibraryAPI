79853d380282ce8783ea5a2b18946b3050692f09
Nivea Stelmam <mand458@hotmail.com>
2025-03-25 11:41:45 -0300

    test not recommended to save an author

--- src/main/java/io/github/niveastelmam/libraryapi/LibraryapiApplication.java
+++ src/main/java/io/github/niveastelmam/libraryapi/LibraryapiApplication.java
@@ -1,13 +1,31 @@
 package io.github.niveastelmam.libraryapi;
 
+import io.github.niveastelmam.libraryapi.models.Author;
+import io.github.niveastelmam.libraryapi.repositories.AuthorRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.LocalDate;
 
 @SpringBootApplication
 public class LibraryapiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LibraryapiApplication.class, args);
+		var context = SpringApplication.run(LibraryapiApplication.class, args);
+		AuthorRepository repository = context.getBean(AuthorRepository.class);
+
+		exampleSaveRecord(repository);
+
 	}
 
+	public static void exampleSaveRecord(AuthorRepository authorRepository){
+		Author author = new Author();
+		author.setName("George Orwell");
+		author.setNationality("British");
+		author.setDateBirth(LocalDate.of(1903, 6, 25));
+
+		var authorSaved = authorRepository.save(author);
+		System.out.println("Author saved: " + authorSaved );
+
+	}
 }
