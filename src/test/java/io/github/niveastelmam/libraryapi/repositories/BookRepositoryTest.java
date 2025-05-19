c683c2d41864fea787022ea009141479dd9faf52
Nivea Stelmam <mand458@hotmail.com>
2025-05-19 15:32:08 -0300

    Example Cascade

--- src/test/java/io/github/niveastelmam/libraryapi/repositories/BookRepositoryTest.java
+++ src/test/java/io/github/niveastelmam/libraryapi/repositories/BookRepositoryTest.java
@@ -21,6 +21,8 @@ class BookRepositoryTest {
     @Autowired
     AuthorRepository authorRepository;
 
+
+    // Searches for an existing Author in the database by ID and associates it with the Book
     @Test
     void saveTest(){
          Book book = new Book();
@@ -35,6 +37,48 @@ class BookRepositoryTest {
          book.setAuthor(author);
 
          bookRepository.save(book);
+    }
+
+    @Test
+    void saveAuthorAndBookTest(){
+        Book book = new Book();
+        book.setIsbn("12345-67853");
+        book.setPrice(BigDecimal.valueOf(100));
+        book.setGender(BookGenre.FICTION);
+        book.setTitle("Uzumaki");
+        book.setPublicationDate(LocalDate.of(2018, 5, 22));
+
+        Author author = new Author();
+        author.setName("Junji Ito");
+        author.setNationality("Japanese");
+        author.setDateBirth(LocalDate.of(1947, 9, 21));
+
+        authorRepository.save(author);
+
+        book.setAuthor(author);
+
+        bookRepository.save(book);
+    }
+
+    // Example Cascade: create a new Author, Book association and save only the Book
+    @Test
+    void saveCascadeTest(){
+        Book book = new Book();
+        book.setIsbn("12345-67853");
+        book.setPrice(BigDecimal.valueOf(100));
+        book.setGender(BookGenre.FICTION);
+        book.setTitle("Revolução dos bichos");
+        book.setPublicationDate(LocalDate.of(2018, 5, 22));
+
+        Author author = new Author();
+        author.setName("George Orwell");
+        author.setNationality("British");
+        author.setDateBirth(LocalDate.of(1947, 9, 21));
+
+
+        book.setAuthor(author);
+
+        bookRepository.save(book);
     }
 
 }
